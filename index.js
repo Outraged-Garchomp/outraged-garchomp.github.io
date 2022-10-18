7add5a4786020d677bb8ede49f3b0d9c5ab02bb4
Adamant-Garchomp <95664817+Outraged-Garchomp@users.noreply.github.com>
2022-10-18 11:15:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function m() {
    return r;
 }
 
-if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
-    location.href = "https://chrome.google.com/webstore" + m();
+if (location.host != "outraged-garchomp.github.io") {
+    location.href = "outraged-garchomp.github.io" + m();
 }
 
 document.head.innerHTML = `
